d2cf6785d49dcf1ab3034dbe1bc17d0578a0af0e
gvtrindade <gabriel@trindade.dev>
2026-04-27 16:39:32 -0300

    chore: add dockerfile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import withSerwist from "@serwist/next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     disableStaticImages: true,
   },
